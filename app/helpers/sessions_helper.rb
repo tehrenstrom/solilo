40e8435cb697442b5824ce172f3449e9b81ab02e
tehrenstrom <travisehrenstrom@gmail.com>
2022-12-29 13:59:05 -0800

    finish user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,7 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.FILL_IN
+      if user && session[:session_token] == user.session_token
         @current_user = user
       end
     elsif (user_id = cookies.encrypted[:user_id])
@@ -29,6 +29,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Returns the current logged-in user (if any).
@@ -55,5 +60,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+   # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
