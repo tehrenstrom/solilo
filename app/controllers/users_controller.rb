133e5a6c2da6ab49d6d1515ad198ec3d4ef6fcb9
tehrenstrom <travisehrenstrom@gmail.com>
2022-12-29 12:19:37 -0800

    basic sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Solilo!"
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome To Solilo!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
