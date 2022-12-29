97a20cec48c0c9d8b304e185ba8b9d75ba0904b4
tehrenstrom <travisehrenstrom@gmail.com>
2022-12-29 13:03:06 -0800

    remember me sessions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Set up all fixtures in test/fixtures/*.yml.
   fixtures :all
 
   # Returns true if a test user is logged in.
@@ -16,5 +15,18 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
